fb202fe46d2e793c0f11043a28f903b9c9989eaa
Alexey Tudarov <alexeitudarov@yandex.ru>
2026-06-30 21:07:58 +0700

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,6 +2,9 @@ package daysteps
 
 import (
 	"time"
+	"strings"
+	"strconv"
+	"errors"
 )
 
 const (
@@ -11,10 +14,53 @@ const (
 	mInKm = 1000
 )
 
+//parsePackage принимает строку и возвращает количество шагов, продолжительность активности и ошибку.
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	part := strings.Split(data, ",") //Разбиваем строку по запятой на две части
+	
+	//Проверяем, что получилось две части.
+	if len(part) != 2 {
+		return 0, 0, errors.New("неверный формат: ожидается строка вида 'число время'")
+	}
+
+	//Количество шагов типа int.
+	step, err := strconv.Atoi(part[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка парсинга числа: %w", err)
+	}
+
+	//Длительность ходьбы типа time.Duration.
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка парсинга времени: %w", err)
+	}
+
+	return step, duration, nil
 }
 
+//DayActionInfo принимает количество шагов, продолжительность ходьбы, вес и рост и возвращает количество шагов, дистанцию и количество калорий в нужном формате.
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	
+	//Получаем данные о количестве шагов, продолжительности прогулки и наличии ошибки
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	//проверяем, что количество шагов больше 0
+	if steps <= 0 {
+		return ""
+	}
+
+	//Пройденная дистанция в метрах.
+	distM := float64(steps) * stepLength
+
+	//Пройденная дистанция в километрах.
+	distKm := distM / float64(mInKm)
+
+	//Количество калорий, потраченных при ходьбе.
+	calories := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", steps, distKm, calories)
 }
