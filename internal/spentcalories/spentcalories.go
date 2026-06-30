998f2f2694deccf90e54d8748e6f23f8bca9c818
Alexey Tudarov <alexeitudarov@yandex.ru>
2026-06-30 22:54:00 +0700

    добавил проверку в parseTraining2

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -43,6 +43,10 @@ func parseTraining(data string) (int, string, time.Duration, error) {
 		return 0, "", 0, errors.New("шаги <= 0")
 	}
 
+	if duration <= 0 {
+		return 0, "", 0, errors.New("продолжительность должна быть больше 0")
+	}
+
 	return step, part[1], duration, nil
 }
 
